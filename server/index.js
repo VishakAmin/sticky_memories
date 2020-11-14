fe92480507bb59b390645623cc9cd84e419b04b8
VishakAmin <aminvishak@gmail.com>
2020-11-14 17:12:10 +0530

    Deployment

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/post.js'
 
 const app = express();
-
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
@@ -14,11 +14,13 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
+app.get('/', (req, res) => {
+    res.send("Hello to Stick Memories API")
+})
 
-const CONNECTION_URL = 'mongodb+srv://vishak:Vishak@123@cluster0.7vudp.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log('Server Running')))
     .catch((error) => console.log(error.message));
 
