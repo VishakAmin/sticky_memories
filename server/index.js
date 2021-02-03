3af6f50cf0b102a990c2daf1b0c69754cc6a9800
VishakAmin <aminvishak@gmail.com>
2021-02-03 23:04:12 +0530

    Added

--- server/index.js
+++ server/index.js
@@ -63,8 +63,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-const CONNECTION_URL = "mongodb+srv://vishak:Vishak@123@cluster0.7vudp.mongodb.net/<dbname>?retryWrites=true&w=majority
-";
+const CONNECTION_URL = "mongodb+srv://vishak:Vishak@123@cluster0.7vudp.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
