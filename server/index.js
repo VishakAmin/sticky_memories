9d8e45c254f9f342dc15ae81884b4d31265312dc
VishakAmin <aminvishak@gmail.com>
2021-02-03 23:15:58 +0530

    Added

--- server/index.js
+++ server/index.js
@@ -61,6 +61,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use("/posts", postRoutes);
+
 app.use("/user", userRoutes);
 
 const CONNECTION_URL = "mongodb+srv://vishak:Vishak@123@cluster0.7vudp.mongodb.net/<dbname>?retryWrites=true&w=majority";
